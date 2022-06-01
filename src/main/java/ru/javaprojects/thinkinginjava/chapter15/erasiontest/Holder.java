eb191df75ea6fdcfa667a3351b94df0d14b733ee
Igor Shlyakhtenkov <ishlyakhtenkov@yandex.ru>
2022-06-01 14:39:31 +0300

    Add chapter 15 task 21

---
+++ src/main/java/ru/javaprojects/thinkinginjava/chapter15/erasiontest/Holder.java
@@ -0,0 +1,16 @@
+package ru.javaprojects.thinkinginjava.chapter15.erasiontest;
+
+public class Holder<T> {
+//    private T element = new T();
+    private T element;
+
+    public void test() {
+//        if (T instanceof String) {
+//
+//        }
+    }
+
+
+
+
+}
